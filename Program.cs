1b074d0f18335c99c71d667d1ca839d5927d2287
Sibanbis <104072012+Sibanbis@users.noreply.github.com>
2022-05-28 12:19:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using AppRelationModel18;
+namespace HelloApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // пересоздаем базу данных
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                Country usa = new Country { Name = "USA" };
+                Company microsoft = new Company { Name = "Microsoft", Country = usa };
+                Company google = new Company { Name = "Google", Country = usa };
+                db.Companies.AddRange(microsoft, google);
+                User tom = new User { Name = "Tom", Age = 36, Company = microsoft };
+                User bob = new User { Name = "Bob", Age = 39, Company = google };
+                User alice = new User { Name = "Alice", Age = 28, Company = microsoft };
+                User kate = new User { Name = "Kate", Age = 25, Company = google };
+                db.Users.AddRange(tom, bob, alice, kate);
+                db.SaveChanges();
+            }
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var groups = db.Users.GroupBy(u => u.Company.Name).Select(g => new
+                {
+                    g.Key,
+                    Count = g.Count()
+                });
+
+                Console.WriteLine("Группировка");
+                foreach (var group in groups)
+                {
+                    Console.WriteLine($"{group.Key} - {group.Count}");
+                }
+            }
+
+            Console.Read();
+        }
+
+    }
+    }
